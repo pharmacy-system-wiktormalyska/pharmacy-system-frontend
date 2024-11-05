28bc0f5398c2a4a0e98f264fcaee6496c59ce502
Wiktor Małyska <wiktormalyska03@gmail.com>
2024-11-05 13:56:45 +0100

    Added logout functionality and requirement to login to see mainpage

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,11 +2,21 @@ import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Main, HeaderText, Sidebar, Logo, TabButton, AccountSection, AccountName, LogoutButton } from "../values/components.ts";
 import {useEffect} from "react";
+import {useAuth} from "../api/AuthContext.tsx"
+import {useNavigate} from "react-router-dom";
 
 export const MainPage = () => {
+    const {user, logout} = useAuth()
+    const navigate = useNavigate();
 
+    const handleLogout = () => {
+        logout()
+        navigate('/login')
+    }
     useEffect(() => {
-
+        if (user === null) {
+            navigate('/login')
+        }
     })
 
     return (
@@ -22,7 +32,8 @@ export const MainPage = () => {
                 <TabButton>Tab 3</TabButton>
                 <AccountSection>
                     <AccountName>example@mail.com</AccountName>
-                    <LogoutButton>Logout</LogoutButton>
+                    <LogoutButton onClick={handleLogout}>Logout</LogoutButton>
+
                 </AccountSection>
             </Sidebar>
         </>
