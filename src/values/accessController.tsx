6950625df2270d0d5d6d8c4ee970be5a56df8db8
wiktormalyska <wiktormalyska03@gmail.com>
2024-11-18 19:02:24 +0100

    Removed auth

--- src/values/accessController.tsx
+++
@@ -1,9 +0,0 @@
-import { RoleAndAccess } from './types.ts';
-
-export const accessController: RoleAndAccess[] = [
-    {
-        role: 'admin',
-        panelKey: ['department', 'main', 'warehouse']
-    }
-]
-
